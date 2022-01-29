1d3c06e74364e64785c471a0b746e0abc3bbf1d8
KatieKeyboard <88992523+KatieKeyboard@users.noreply.github.com>
2022-01-28 23:55:05 -0800

    test(piano): add spec and fixture files

---
+++ piano.spec.js
@@ -0,0 +1,20 @@
+//A custom Cypress command to click on a piano key, followed by a waiting period. This type of command would typically be referenced from a cypress/support/commands.js file.
+Cypress.Commands.add('piano', (key, wait) => {
+	cy.get('[data-note=' + key + ']').click();
+	cy.wait(wait);
+});
+
+describe('Musicca Virtual Piano', () => {
+	it('Plays a Theme Song from Musical Notes', () => {
+		cy.visit('https://www.musicca.com/piano');
+
+		cy.intercept('/lydfiler/piano/63.mp3').as('tone63');
+		cy.wait('@tone63').its('response.statusCode').should('eq', 200);
+
+		cy.fixture('composition.json').then((arrangement) => {
+			for (const property in arrangement) {
+				cy.piano(arrangement[property].key, arrangement[property].wait);
+			}
+		});
+	});
+});
