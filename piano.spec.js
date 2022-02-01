da3b74bbea2600da4fcbfa549c6058c570adfc7d
KatieKeyboard <88992523+KatieKeyboard@users.noreply.github.com>
2022-01-31 16:52:59 -0800

    test(piano): handle application error

--- piano.spec.js
+++ piano.spec.js
@@ -1,3 +1,10 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+	// We expect Uncaught TypeErrors in the application under test
+	if (err.message.includes('> Script error')) {
+		return false;
+	}
+});
+
 //A custom Cypress command to click on a piano key, followed by a waiting period. This type of command would typically be referenced from a cypress/support/commands.js file.
 Cypress.Commands.add('piano', (key, wait) => {
 	cy.get('[data-note=' + key + ']').click();
@@ -6,7 +13,7 @@ Cypress.Commands.add('piano', (key, wait) => {
 
 describe('Musicca Virtual Piano', () => {
 	it('Plays a Theme Song from Musical Notes', () => {
-		cy.visit('https://www.musicca.com/piano');
+		cy.visit('https://www.musicca.com/piano/');
 
 		cy.intercept('/lydfiler/piano/63.mp3').as('tone63');
 		cy.wait('@tone63').its('response.statusCode').should('eq', 200);
